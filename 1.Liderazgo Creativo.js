255670af6e2b98b055fa5c577c8f5eea76f9121b
renan <rchacaliaza@asf.pe>
2020-12-18 09:02:07 -0500

    first commit

---
+++ 1.Liderazgo Creativo.js
@@ -0,0 +1,413 @@
+
+
+var A = "NUEVOS PARADIGMAS DE ORGANIZACION "
+
+var a1  = "Las organizaciones hoy"
+var a1v = `
+Desde la revolución cognitiva de los sapiens - la realidad dual (Yuval Noah) las corporaciones forman parte de la realidad imaginaria. 
+Las organizaciones construyen mensajes ficticios, que ayudan a las personas a unirse a ellas.
+.
+Organización: grupo social foperfrmado por personas, tareas y administración, que interactúan en el marco de una estructura para 
+umplir con sus objetivos.
+.
+Las organizaciones modernas han traído progreso a la humanidad en tan solo dos siglos. Son el vehículo de la colaboración humana.
+.
+💔 Por alguna razón, el modelo actual de organización se está rompiendo:
+💔 Estamos desilusionados
+💔 Engagement muy bajo (15%)
+💔 Management frustrado
+.
+Con las organizaciones de hoy, las personas no tienen el mismo nivel de enganche, y los líderes están frustrados porque no logran 
+cambiar esto para que se sientan valoradas, inspiradas, y quieran dejar el máximo en las organizaciones a las que pertenecen.
+.
+“El mayor peligro en tiempos turbulentos no es la turbulencia; es actuar con la misma lógica que antes” - Peter Drucker.
+.
+Esto se debe a que estamos enfrentándonos a nuevos problemas, tratando de resolverlos con las mismas reglas del pasado;
+ y estamos en un punto en el que necesitamos cambiar por completo el entendimiento que tenemos de cómo interactuamos.
+.
+📘 Frederic Laloux - Reinventar las Organizaciones (Guía Práctica Ilustrada)
+.
+Tipos de Organizaciones
+
+🐺 IMPULSIVAS (rojas)
+🐺 Autoridad de mando
+🐺 Ejercicio de poder constante
+🐺 Se agrupan por miedo
+🐺 Reactivas
+🐺 Poca planeación / muchas adaptabilidad
+.
+⬇ CONFORMISTAS (ámbar)
+⬇ Paternalista - Autoritario
+⬇ Roles formales y jerárquicos (de arriba hacia abajo)
+⬇ Control Top-Down
+⬇ Estabilidad y rigor (procesos - eficiencia y resultados)
+⬇Ejemplo: colegios e incluso empresas familiares.
+.
+🎖 POR LOGROS (naranjas)
+🎖 Orientadas a tareas y logros (ego - líder todopoderoso en el que radica todo el impacto)
+🎖 Enfocadas en crecer y ganar
+🎖 Gestión por objetivos
+🎖 Innovación
+🎖 Meritocracia
+
+👁‍🗨 Estamos viviendo en un momento en el que no podemos hablar (todavía) de organizaciones que van
+a crecer siempre y sin medida, estamos entrando a nuevos retos a nivel planetario y como sociedad;
+ y cuando tratamos entender cuál debería ser el siguiente esquema por el cual nos organizamos, 
+ simplemente no podemos porque no estamos preparados para imaginar un modelo nuevo.
+
+ ¿Puede ser que nuestra visión actual del mundo limita la manera en la que pensamos en organizaciones?
+Por supuesto, la mayoría de seres humanos se limita a tomar desiciones con respecto al contexto inmediatamente
+ al rededor de él, debemos ir más allá, comprender el comportamiento de las personas, cada uno puede ser un líder
+ , si día a día rompe con el síndrome del impostor que hay en muchas organizaciones, ciudades e incluso países.
+
+¿Podemos imaginar un modelo más significativo que responda al cambio de era si cambiamos nuestro sistema de creencias?
+El tipo de organizaciones es conveniente al contexto, la mayor habilidad del ser humano, es su capacidad de adaptarse
+ rapidamente a los cambios en la sociedad. El covid-19 supone un nuevo hito en el mundo, nos obliga a pensar diferente,
+  solo aquellas organizaciones que cambien con respecto a las necesidades del mercado tendrán el éxito por encima de las demás.
+   Habrá un mejor modelo, ya que muchos paradigmas de la sociedad se han roto. Lo cual es bueno.
+          `
+
+var a2  = "Relación - Empleado - Organización- Lider"  
+var a2v = `El éxito se mide con el modelo de engagement.
+Solo el 15% de los empleados del mundo están enganchados en el trabajo.
+La relacion que tienen los empleados con las organizaciones modernas han cambiado.
+
+Engagement tiene que ver con la felicidad y la satisfacción con nuestro trabajo.
+
+Ahora estamos entrando al esquema de la Experiencia.
+Donde la persona se involucra debido a los valores que tenga la empresa.
+Para que la persona se entregue como realmente es, con sus valores y experiencia.
+
+La relación que tiene el empleado con la empresa es 70% responsabilidad del lider.
+Estamos en tiempo de transición.
+
+Tipos de Organizaciones
+
+Organizaciones Pluralistas
+Organizaciones jerárquicas, de distribuir responsabilidades, orientada a motivar los empleados.
+
+Las organizaciones Evolutivas
+Organización sin jefes, una organización donde todos son lideres.
+Con propósito evolutivo.
+
+Tenemos que cambiar las personas para cambiar.
+
+EL EGO ES EL ENEMIGO
+Estamos en una crisis de valores y principios. Estamos trabajando con propósitos egoistas.`
+
+var a3  = "Estilos de liderazgo"  
+var a3v = ` Encabeza / Dirige / encamina un grupo hacia un proposito comun
+            Persona que encabeza un grupo o mov social /Orienta un grupo
+            Transformacion / Autentico / Al servicio
+
+`
+
+var B = "ESTILOS DE LIDERAZGO "
+
+var b1  = "Liderazgo Transformacion"
+var b1v = `La practica en donde lidres y seguidores provocan reciprocamente
+al avanzar a mayores niveles de moral y motivación.
+
+El rol de este lider es ayudar a desarrollar las habilidades individuales para
+que lleguen al mismo propósito.
+
+Promueva intencionalmente un propósito y visión.
+A buscar nuevas oportunidades.
+Es una persona que inspira y motiva.
+Los lideres transformacionales lideran con el ejemplo.
+Los miembros verán una persona que confia en los demás.
+
+Los lideres transformacionales son aquellos que estimulan e inspiran a
+sus seguidores tanto a conseguir resultados extraordinarios, como, en el proceso a desarrolalr
+su propia capacidad de liderazgo.
+
+¿Cómo ser un lider transformacional?
+
+Visión clara y positiva
+Pasión e impacto colectivo.
+Habilidades
+Checar tu ego
+Auto Inspiración —> Como te motivas?
+Tomar Riesgos
+Tomar Decisiones en base a conocimiento
+Buscar bien común de la organzación
+Inspirar a otros
+Promover nuevas ideas
+Ser adaptable, estar atento a las situaciones
+Ser proactivo, estan activos
+Liderar con visión y determinan con objetivos realistas y con impacto
+y lo que quieran que las personas sean comprometidas, y que promueva el bienestar del grupo.
+
+La forma de desarrollar estas habilidades es comenzando a practicarlas en el diario vivir.`
+var b2  = "Liderazgo Autentico"
+var b2v = `
+El rol del líder en la actualidad impacta dentro y fuera de la organización. El liderazgo auténtico apunta a comprometerse con el autodesarrollo de las personas.
+Desde la antigua Grecia, la autenticidad se vincula con el conocimiento propio y el control sobre nosotros mismos.
+Hoy, en una organización, hablar de autenticidad en el liderazgo, es hablar también del reflejo que tiene la organización en el exterior a través de sus personas, cómo se define la organización a partir de cómo sus colaboradores se definen a sí mismos, cómo ejecutan su rol y cómo se percibe su responsabilidad desde fuera.
+Un punto fundamental es el reconocimiento de quién realmente somos como persona.
+Es maximizar nuestras características como personas y que se conviertan en una cualidad positiva para el modelo de liderazgo.
+Un líder auténtico es quien es, en todo momento, y eso se convierte en una de sus principales fortalezas.
+Un líder auténtico actúa a su máxima expresión, y da certeza y seguridad ante todo. No quiere decir que tenga todas las respuestas, pero sí que se muestra entera (no está actuando).
+
+Características del líder auténtico:
+    Ser auto-consientes y genuinos (no ocultan sus fallas o vulnerabilidades).
+Enfocados en propósito y bien común.
+    Se guían por el corazón, son apasionados (conectan a nivel personal).
+Van por el largo plazo (se enfocan en el valor compartido).
+
+¿Cómo ser un líder auténtico?
+    Conócete a ti mismo.
+    Sé genuino y transparente.
+    Sé justo y aprende desde múltiples perspectivas.
+    Siempre haz lo correcto.
+
+Principios que rigen al líder auténtico:
+    Ética.
+    Poder distribuido.
+
+Comunicación en todas direcciones (escuchan más de lo que hablan).`
+var b3  = "Liderazgo al Servicio"
+var b3v = `
+Una persona que trata satisfacer las necesidades de una tercera persona.
+
+El concepto de liderazgo al servicio.
+El lider es quien sirve primero, promueve todo a servir.
+El lider va liderar sirviendo a los demás.
+De los buenos lideres no nota su existencia.
+A los peores lideres les tienes miedo.
+
+El lider sirve a traves de la ayuda y el desarrollo de otros.
+Enfocadas en el impacto que tiene esto sober las personas menos privilegiadas.
+Reta el modelo jerárquico, se concentra en distribuir el poder.
+Un grupo de lideres. Se enfoca en el bien común.
+
+Este liderazgo es un estilo de compartamiento. El lider escucha activamente,
+tiene empatia, que aprenda de los demás, conocer las fortalezas y debilidades del resto.
+Busca que todos tengan una buena seguridad, que tengan las herramientas para mantener los demás seguros.
+Los lideres al autoconconocerse.
+Los lideres tiene la capacidad de imaginar el futuro. De construir comunidad.
+`
+
+var C = "LIDERAZGO CREATIVO "
+
+var c1  = "Que es"  
+var c1v = `El lider creativo es la progresión d elos demás estilos de liderazgo, que incorpora
+demás de valores morales, influencia inspiracional.
+
+4 elementos importantes
+
+Autoconocimiento
+Desde nuestra perspectiva como desde la de otros.
+Creatividad
+Como resolvemos problemas.
+Pensamiento sistémico y prospectivo
+Como entendemos el contexto, cuales son las creencias que nos limitan.
+Resilencia.
+¿Quién soy? ¿Qué mueve mis comportamientos?
+Cómo resolvemos nuestros problemas.
+
+En la resilencia, cuales son los miedos, de donde viene la energia.
+
+Los lideres creativos son exploradores, ponen parte de su atención en su exploración.
+Que permita retar las propias creencias.
+
+“Vive en el futuro, luego construye lo que hace falta” Anticiparse!
+Los lideres creativos tienen un propósito generoso y actúan con pasión.`
+var c2  = "Qué podría impedirte ser un líder creativo" 
+var c2v = `No conocernos, no conocer a nuestras limitaciones y no conocer nuestros paradigmas.
+.
+"Para transformar a las organizaciones, primeros nos tenemos que transformar a nosotros mismos."
+Para transformarnos a nosotros mismos tenemos que entender qué es lo que limita nuestra transformación.
+.
+🚫Creencias limitantes: Es un juicio sobre nosotros mismo o el mundo que nos rodea.
+.
+Hay creencias que son positivas y nos mantienen proactivos, pero hay otras que nos limitan.
+ Las creencias se construyen a lo largo de nuestra vida, la experiencia y la influencia de otros.
+.
+Las creencias y las experiencias pueden estar en un 👍 círculo virtuoso o en un 👎 círculo vicioso;
+ por ejemplo: la predisposición que tenemos a buscar y encontrar bondad en las personas… o la
+  predisposición que tenemos a buscar en las personas una intención malévola oculta, lo cual
+   generaría desconfianza. Depende de cómo nos programemos.
+.
+Tres tipos diferentes de creencias limitantes más comunes
+
+Desesperanza (creer que el objetivo no se puede alcanzar nunca)
+Impotencia (creer que el objetivo es alcanzable, pero creer que yo no pueda alcanzarlo)
+Ausencia de mérito (crees que no merezco abordar ese objetivo)
+.
+El reto está en entender de dónde vienen estas creencias limitantes, para ellos existe un ejercicio:
+👨‍💻 Reframing - Reenmarcamiento. La meta de esta herramienta radica en preguntarnos ¿estos pensamientos 
+negativos de dónde vienen y cuál es el soporte de los mismos?
+.
+¿Cuántas veces al día tienes pensamientos negativos sobre ti mismo, tu trabajo o tus capacidades? 
+¿Qué cosas negativas te dices? ¿Qué cosas no puedes lograr hoy en función de esas cosas negativas 
+que te dices?
+.
+👁‍🗨 Ojo con la disonancia cognitiva: ¿en dónde dices tener una creencia y te encuentras actuando 
+de una forma contraria? ¿En dónde hay esa tención o incomodidad?
+
+El canva de reenmarcamiento es un diagrama de 5 casillas, en ella se hace lo siguiente:
+
+Paso 1: Identificar creencia limitante → redactar diversas creencias negativas personales o de la
+ organizaciones que consideres limitan u obstaculizan.
+Paso 2: Priorizar → elige una de esas creencias y colócala en el centro del cavas.
+Paso 3: Profundizar → formula creencias adicionales que le den soporte a la creencia limitante, 
+¿por qué creo en esto? ¿qué hace que esta creencia sea verdadera?
+Paso 4: Transformarlas → redacta el radical opuesto de cada una de ellas. Mientras más extremo 
+sea el opuesto mejor. No importa que den risa, la idea es salir de esa línea de pensamiento que
+ no contribuye positivamente.
+Paso 5: Reprogramar → redacta la creencia central y llévala a su nuevo opuesto.
+
+ Vamos a darnos cuenta que muchas de las creencias están conectadas a miedos o emociones; lo que
+ debemos hacer es postular oportunidades, dándole la vuelta a las creencias de soporte negativas
+ (ejemplo: mi organización es resiliente a la crisis y siempre encontraremos nuevas formas de
+ entregar valor).`
+
+var D = "COMO PIENSA UN LIDER CREATIVO "
+
+var d1  = "Pensamiento Sistematico"  
+var d1v = `Es una habilidad del abordaje holistico de analisis que se enfoca en como todas las cosas funcionan.
+
+Un sistema es un conjunto de elementos que es más que la suma de las partes.
+Elementos
+Interconecciones
+Propósitos.
+
+¿Cómo pensar en sistemas?
+Para que sirve cada pieza y como se interrelaciona.
+
+El pensamiento Sistémica que se utiliza para entender sistemas, predecir sus comportamientos, e idear
+modificaciones que produzcan los efectos deseados.
+
+Habilidades
+
+Explorar limites, entender todos los involucrados en el sistema.
+Incorportar multiples perspectivas
+Identificar interrelaciones.
+Una herramienta es un mapa mental, ya que sirve para identificar las caracteristicas e influencias.
+Entender todas las piezas que se interrelacionan. Entre si.`
+var d2  = "Pensamiento Prospectivo" 
+var d2v = `La capacidad prospectiva nos permite anticiparnos a las cosas. El problema radica en tener todos los elementos a nuestro alrededor y no saber aprovecharlas, no tener visión.
+.
+Entorno VUCA
+Volátil - Incierto (Uncertain) - Complejo - Ambiguo
+El acrónimo se usó por primera vez en ‘‘teoría del liderazgo’’ de Warren Bennis y Burt Nanus en 1987.
+.
+La observación del entorno a largo plazo con la intención de identificar anticipadamente aquellos aspectos que tendrán un gran impacto en nuestro entorno, ya sea social, tecnológico o económico.
+.
+🌄 Horizon Scanning
+
+Escanear el horizonte es un método sistemático que nos permite identificar potenciales causas de lo incierto, asegurar
+ la preparación para el cambio, capitalizar las oportunidades emergentes y sobrevivir amenazas.
+
+1. definir el marco dentro del cual vamos a actuar (cuál es el problema), y es importante explorar sin límites al principio, 
+aunque en bueno limitarla para evitar el exceso de información y entender realmente lo que queremos aprender.
+
+2. construir una tabla básica en donde se definen los ejes en los cuáles se explorará. Recomienda el formato STEEPLE (señales desde perspectivas ambientales, tecnológicas, sociales, políticas, económicas, legales y éticas).
+
+3. agrupar las señales e información emergente es tópicos que sean transversales, crear grupos de información según lo que tengan en común (interconexiones).
+
+4. postular los nuevos conocimientos en hallazgos: incertidumbres, situaciones emergentes o en tendencias.
+.
+🏗 Construcción de Escenarios
+
+Aprovechar el escaneo de horizontes es poder construir escenarios.
+
+La construcción de escenarios sirve para dibujar alternativas posibles e identificar los acontecimientos en el tiempo con el objetivo de entender cuál de todas esas alternativas se va convirtiendo en más probable.
+
+La construcción de escenarios permite tomar decisiones, medir los efectos de las tendencias o cuestiones emergentes para tomar una decisión o corregir el rumbo, la intención es acercarse al escenario más favorable.
+
+El proceso debe seguir cuatro grandes actividades.
+
+Identificar fuerzas de influencia o cambio, (lo cual inicia en el escaneo de horizonte).
+Identificar principales incertidumbres, (con ello empezamos a imaginar).
+Diseñar posibles escenarios.
+Discutir las implicaciones en el tiempo.
+`
+var d3  = "Pensamiento Critico" 
+var d3v = `Como llegamos a saber lo que sabemos, a pensar lo que pensamos,
+como nuestros comportamientos estan modelados.
+
+El pensamiento critico nos ayuda a tener claridad, profundidad, justicia.
+
+Logramos esto a traves de construir conocimientos.
+
+Habilidades
+
+Capacidad de Reflexión
+Flexibilidad
+Lógica y detección de sesgos
+Capacidad de duda de las cosas
+Motivación y curiosidad
+**Cómo ponerlo en práctica
+**
+
+Hacerse preguntas
+Recopilas, analizas e interpretas efectivamente
+Piensas de forma abierta e inclusiva
+Te comunicas efectivamente
+El pensamiento critico permite afrontar con valentia y convicción
+los diferentes dilemas de la vida.`
+
+var E = "PROPOSITO PERSONAL | AUTOCONOCIMIENTO"
+
+var e1  = "Cómo diseñar tu propósito"  
+var e1v = `Hay que entender, que primero hay que aprender a liderarnos a nosotros mismos,
+trabajar juntos con impacto e inspiración y posteriormente dejar una huella en dejar el mundo.
+
+La meditación sirve para conocerse internamente y la secuencia de nuestros pensamientos.
+
+Al definir un propósito hay que conocer nuestros valores y principios.
+Todas las personas somos una casa con 4 habitaciones:
+-Fisico
+-Mental
+-Emocional
+-Espiritual
+
+Hay que visitar las 4 habitaciones durante el dia.
+Considerar las 4 areas para ser un ser más completo.
+
+Preguntarnos constantemente que es lo queremos de la vida.
+El autoconocimiento es importante.
+El autoconicimiento puede ser interno y externo ( como nos ven los otros ).
+Centrar nuestros pensamientos en lo que queremos y en lo que nos dará crecimiento.
+Al escuchar opiniones externas hay que escuchar algo balanceado.
+
+Lo que buscamos es ser lideres autoconcientes, el poder retar a los supuestos y como estos
+se pueden convertir en oportunidades.
+
+Diseñar nuestro propósito
+-¿Quién soy?
+
+¿Qué es lo que hago?
+¿Por qué hago lo que hago?
+¿ Para quién hago lo que hago?`
+var e2  = "Tus Valores y principios" 
+var e2v = `
+¿Cuales son tus valores?
+Son las creencias centrales que dirigen tu comportamiento.
+
+¿Cuáles son tus principios?
+Son las reglas con las que regimos nuestro comportamiento.
+
+Haz una lista de valores y principios.
+
+Mis Valores:
+Servicio, apoyo, responsabilidad, honestidad, justicia, empatía, respeto y objetividad.
+Mis Principios:
+Solidaridad, cooperación, pertenencia social, mejora continua, uso adecuado de los bienes,
+ adaptación al cambio, igualdad de oportunidades y respeto mutuo.
+`
+var e3  = "Proceso Reatrolimentacion" 
+var e3v = `Que sabemos del líder?
+Que queremos saber del líder?
+Que debería saber el líder de nosotros como equipo?
+Que nos preocupa del líder?
+Que expectativas tenemos del líder?
+Que queremos que el líder deje de hacer, empiece a hacer y siga haciendo?
+Que valoraciones tenemos sobre nuestro propósito, estrategia y plan?
+Recibir>Reflexionar>Responder`
+var e4  = "" 
+var e4v = ``
+
+
